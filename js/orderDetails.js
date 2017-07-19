c380edf43aa6afccda3563866c5f32bb28a397b4
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2017-07-19 15:23:07 +0800

    订单详情页面

---
+++ js/orderDetails.js
@@ -0,0 +1,14 @@
+//仿单选按钮效果
+$(".click-radio").click(function(){
+    // alert(123);
+    $(this).find(".found-radio").addClass("radio-center");
+    $(this).siblings(".click-radio").find(".found-radio").removeClass("radio-center");
+});
+//还款弹层消失
+$(".close-click").click(function(){
+    $(this).parents(".makesure-repay").css("display","none");
+});
+//还款弹层出现
+$(".repayment-liji").click(function(){
+    $(this).parents(".order-detail-list").siblings(".makesure-repay").css("display","block");
+});
