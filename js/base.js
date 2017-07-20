d5755a80cac66be43a5b574c4f37e2652d8c7253
Aline3220 <874024374@qq.com>
2017-07-20 11:31:11 +0800

    add 弹框方法

--- js/base.js
+++ js/base.js
@@ -2,4 +2,10 @@ $(function(){
 	$(".return span").click(function(){
    		window.history.go(-1);
 	});
+
+	function fixCon(img,con){
+		var html='<div class="fix"><div class="fix-tishi"><img src="' + img +'" alt=""><div class="tishi-con">' + con +
+		'</div></div></div>'
+	}
+
 })
