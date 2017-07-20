5ea097d565530c4deef20296efeb5fe0fed0b7b6
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2017-07-20 09:35:47 +0800

    update

---
+++ js/MyStaging.js
@@ -0,0 +1,5 @@
+//顶部状态选项栏未读消息中屏适应
+var itemWidth = $(".staging-item").width();
+var readRight = itemWidth * ;
+console.log(readRight);
+$(".staging-item").children(".unread").css({right:readRight});
