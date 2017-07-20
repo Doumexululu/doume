bc7da1fc7033aec83d979fd2c906d4365599aa71
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2017-07-20 16:07:24 +0800

    提现页面选择银行卡替换

--- js/withdraw.js
+++ js/withdraw.js
@@ -24,7 +24,7 @@ $(".off").click(function(){
 //
 $(".money-btn").click(function(){
     var moneys = $(".lave span").html();
-    console.log(moneys);
+    // console.log(moneys);
     if (moneys < 100 || moneys == "") {
         $(".roule1").css("color","red");
         return false;
@@ -32,4 +32,20 @@ $(".money-btn").click(function(){
         $(".enter-money-num").html(" ¥ " + moneys);
         $(".yanzheng").css("display","block");
     }
+});
+//仿单选按钮效果
+$(".click-radio").click(function(){
+    $(this).find(".found-radio").addClass("radio-center");
+    var index =  $(this).index();
+    var imgsrc = $(this).find(".backcard-tit-left img").attr("src");
+    var back = $(this).find(".back-info .back").html();
+    var backid = $(this).find(".back-info .backid").html();
+    $(this).parents(".makesure-repay").siblings(".main-width").find(".backcard-tit-left img").attr("src", imgsrc);
+    $(this).parents(".makesure-repay").siblings(".main-width").find(".backcard-tit-left .back-info span").eq(0).html(back);
+    $(this).parents(".makesure-repay").siblings(".main-width").find(".backcard-tit-left .back-info span").eq(1).html(backid);
+    $(this).siblings(".click-radio").find(".found-radio").removeClass("radio-center");
+});
+//还款弹层消失
+$(".close-click").click(function(){
+    $(this).parents(".makesure-repay").css("display","none");
 });
