313988b12bb9734eb6a937b8af7a0334398ab437
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2017-07-24 17:03:00 +0800

    update

--- js/question-list.js
+++ js/question-list.js
@@ -1,5 +1,5 @@
 $(".questions").click(function(){
-    $(this).find(".flex1").toggleClass("fontcolor-green")
+    $(this).find(".flex1").toggleClass("fontcolor-green");
     $(this).find(".enter-arrow").toggleClass("trans");
     $(this).siblings(".answer").fadeToggle();
 });
