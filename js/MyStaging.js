72c73ca599f4511d0b21213c067f8caa56ba228e
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2017-07-25 16:08:29 +0800

    MyStaging页面增加两个弹窗

--- js/MyStaging.js
+++ js/MyStaging.js
@@ -3,3 +3,12 @@
 // var readRight = itemWidth * ;
 // console.log(readRight);
 // $(".staging-item").children(".unread").css({right:readRight});
+$(".close-uninfo").click(function(){
+    $(this).parents(".uninfo").css("display","none");
+});
+$(".staging-detail-foo").click(function(){
+    $(this).parents(".staging-list").siblings(".uninfo").css("display","block");
+});
+$(".repayment").click(function(){
+    $(this).parents(".staging-list").siblings(".makesure-repay").css("display","block");
+});
