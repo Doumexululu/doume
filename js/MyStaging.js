ef0b6f24baf4c7b3c72be4855fda98fbfe01e810
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2017-08-01 11:57:41 +0800

    后八位显示省略号

--- js/MyStaging.js
+++ js/MyStaging.js
@@ -15,3 +15,11 @@ $(".repayment").click(function(){
     $('body').css("overflow","hidden");
     $(this).parents(".staging-list").siblings(".makesure-repay").css("display","block");
 });
+
+//订单号省略显示
+textcon($(".staging-detail-tit span:first-child"),19);
+function textcon(textcon2,num){
+    var textco = textcon2.text();
+    textco2 = textco.substring(0,num);
+    $(".staging-detail-tit span:first-child").text(textco2+"...");
+};
